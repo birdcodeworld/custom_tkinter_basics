3e36c3bcdc178e5d40759e7e0d114d9dd561fe25
BirdCode888 <birdcode.cali@gmail.com>
2025-04-17 11:23:14 -0500

    Using the sticky argument

--- Basic_window.py
+++ Basic_window.py
@@ -11,7 +11,7 @@ class App(customtkinter.CTk):
 		self.grid_columnconfigure(0, weight = 1)
 
 		self.welcome = customtkinter.CTkButton(self, text = 'BirdCode', command = self.button_welcome)
-		self.welcome.grid(row = 0, column = 0, padx = 20, pady = 20)
+		self.welcome.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = 'ew')
 
 	def button_welcome(self):
 
