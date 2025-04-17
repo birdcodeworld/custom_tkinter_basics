fadac4491400b6cb0bd18cdea03292ad24051690
BirdCode888 <birdcode.cali@gmail.com>
2025-04-17 11:36:02 -0500

    Adding two checkboxes in two columns

--- Basic_window.py
+++ Basic_window.py
@@ -12,6 +12,10 @@ class App(customtkinter.CTk):
 
 		self.welcome = customtkinter.CTkButton(self, text = 'BirdCode', command = self.button_welcome)
 		self.welcome.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = 'ew')
+		self.casual_checkbox = customtkinter.CTkCheckBox(self, text = 'Casual birdwatcher')
+		self.casual_checkbox.grid(row = 1, column = 0, padx = 20, pady = (0, 20), sticky = 'w')
+		self.backyard_checkbox = customtkinter.CTkCheckBox(self, text = 'Backyard birder')
+		self.backyard_checkbox.grid(row = 1, column = 1, padx = 20, pady = (0,20), sticky = 'w')
 
 	def button_welcome(self):
 
