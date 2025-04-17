f251358168c9b342e0c35357579464cc08569196
BirdCode888 <birdcode.cali@gmail.com>
2025-04-17 12:55:19 -0500

    Applying columnspan parameter

--- Basic_window.py
+++ Basic_window.py
@@ -8,14 +8,14 @@ class App(customtkinter.CTk):
 	def __init__(self):
 		super().__init__()
 		self.geometry('900x600')
-		self.grid_columnconfigure(0, weight = 1)
+		self.grid_columnconfigure((0, 1), weight = 1)
 
-		self.welcome = customtkinter.CTkButton(self, text = 'BirdCode', command = self.button_welcome)
-		self.welcome.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = 'ew')
+		self.welcome = customtkinter.CTkButton(self, text = 'BirdCode', width = 200, command = self.button_welcome)
+		self.welcome.grid(row = 0, column = 0, padx = 20, pady = 20, columnspan = 2)
 		self.casual_checkbox = customtkinter.CTkCheckBox(self, text = 'Casual birdwatcher')
-		self.casual_checkbox.grid(row = 1, column = 0, padx = 20, pady = (0, 20), sticky = 'w')
+		self.casual_checkbox.grid(row = 1, column = 0, padx = 20, pady = (30, 20), sticky = 'w')
 		self.backyard_checkbox = customtkinter.CTkCheckBox(self, text = 'Backyard birder')
-		self.backyard_checkbox.grid(row = 1, column = 1, padx = 20, pady = (0,20), sticky = 'w')
+		self.backyard_checkbox.grid(row = 1, column = 1, padx = 20, pady = (30,20), sticky = 'w')
 
 	def button_welcome(self):
 
